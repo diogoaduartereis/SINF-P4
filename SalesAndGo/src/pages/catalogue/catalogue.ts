22a6fddc7f5015307b8fe7d7d6da11fb911bccd3
up201505472 <up201505472@fe.up.pt>
2018-12-14 16:10:24 +0000

    Sorting and filtering working to the fullest

--- SalesAndGo/src/pages/catalogue/catalogue.ts
+++ SalesAndGo/src/pages/catalogue/catalogue.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { PrimaveraProvider } from '../../providers/primavera/primavera';
 
 /**
  * Generated class for the CataloguePage page.
@@ -14,6 +15,7 @@ import { HomePage } from '../home/home';
 @Component({
   selector: 'page-catalogue',
   templateUrl: 'catalogue.html',
+  providers: [PrimaveraProvider],
 })
 export class CataloguePage {
 
@@ -24,46 +26,34 @@ export class CataloguePage {
 
 
   searchQuery: string = '';
-  items: string[];
-  sortValue: string = 'Price';
-  products: string[];
+  items: string[] = [];
+  sortValue: string = 'PVP1';
+  sortType: string = 'Price'
+  products: string[] = [];
   originalHTML: string = '';
-  families: string[];
-  familiesArray: object[];
+  families: string[] = [];
+  familiesArray: string[] = [];
+  originalProducts: string[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", 'http://94.60.211.16:2018/WebApi/token', true);
-    var params = 'username=FEUP&password=qualquer1&company=BELAFLOR&instance=DEFAULT&grant_type=password&line=professional';
-    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    xhttp.send(params)
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+              public primavera: PrimaveraProvider) 
+  {
 
-    let object = this;
+    const access_token = primavera.genAccessToken();
 
-    xhttp.onreadystatechange=function(){
-      if(this.readyState==4 && this.status==200){
-        let response = JSON.parse(xhttp.responseText)
-        const Http = new XMLHttpRequest();
-        const url = 'http://94.60.211.16:2018/WebApi/Administrador/Consulta'
-        Http.open("POST", url);
-        Http.setRequestHeader("Content-type", "application/json; charset=utf-8");
-        Http.setRequestHeader("Authorization", 'Bearer ' + response.access_token);
-        const query = `SELECT A.Artigo, A.Descricao, A.Observacoes, A.familia,
-                       AM.PVP1, VAM.StkActual, AM.Moeda 
-                       from Artigo A INNER JOIN ArtigoMoeda AM 
-                       ON A.Artigo = AM.Artigo join V_INV_ArtigoArmazem VAM 
-                       on A.artigo = VAM.artigo
-                       ORDER BY AM.PVP1`;
-        Http.send(JSON.stringify(query))
-        Http.onreadystatechange=function(){
-          if(this.readyState==4 && this.status==200){
-            object.products = JSON.parse(Http.responseText).DataSet.Table;
-            object.uniq_fast(object.products);
-          }
-        }
-      }
-    }
+    let query = `SELECT A.Artigo, A.Descricao, A.Observacoes, A.familia,
+                  AM.PVP1, VAM.StkActual, AM.Moeda 
+                  from Artigo A INNER JOIN ArtigoMoeda AM 
+                  ON A.Artigo = AM.Artigo join V_INV_ArtigoArmazem VAM 
+                  on A.artigo = VAM.artigo
+                  ORDER BY AM.PVP1`;
 
+    let response = primavera.postRequest(access_token,'/Administrador/Consulta', 200, query);
+    if(typeof response != 'undefined')
+    {
+      this.products = response;
+      this.uniq_fast(this.products);
+    }  
   }
 
   initializeItems() {
@@ -115,6 +105,7 @@ export class CataloguePage {
     this.families = out;
   }
   
+  //FilterBox
   newProducts = [];
   showCheckbox() {
     let alert = this.alertCtrl.create();
@@ -138,86 +129,94 @@ export class CataloguePage {
     alert.addButton({
       text: 'Okay',
       handler: data => {
-        if(this.originalHTML.length == 0)
-          this.originalHTML = document.getElementById('productsListID').innerHTML;
+
+        if(this.originalProducts.length == 0)
+          this.originalProducts = this.products;
         
-        let originalProducts = this.products;
-        let resultingHTML = ""
         for(let i = 0; i < data.length; i++)
         {
-          for(let j = 0; j < this.products.length; j++)
+          for(let j = 0; j < this.originalProducts.length; j++)
           {
-            let familia = originalProducts[j]
+            let familia = this.originalProducts[j]
             if(familia['familia'] == data[i])
             {
               this.newProducts.push(familia);
-              resultingHTML += 
-                `
-                <div class = "item-inner">
-                <div class = "input-wrapper">
-                <!--bindings={
-                  "ng-reflect-ng-for-of": "[object Object],[object Object"
-                }--><ion-item class="item item-block item-md"><div class="item-inner"><div class="input-wrapper"><!--bindings={
-                  "ng-reflect-ng-if": "true"
-                }--><ion-label class="label label-md">
-                  <img src = "http://94.60.211.16:8080/images/` + familia['Observacoes'] +`" style="width:40%; height:40%; display:block; margin-left: auto; margin-right:auto;">
-                  <h1 style="text-align:center; margin:8px;">` + familia['Descricao'] + `</h1>
-                  <button style="float:left; width:50%; height:35px;" ion-button color="light">
-                    <h2 style="margin:auto;"><b>` + familia['PVP1'] + familia['Moeda'] +`&emsp;<ion-icon name="cart" role="img" class="icon icon-md ion-md-cart" aria-label="cart" ng-reflect-name="cart"></b></h2>
-                  </button>
-                  <h2 style = "float:right;margin:12px;">Stock:` + familia['StkActual'] + `</h2>
-                  </div>
-                  </div>
-                `
             }
           }
         }
-        if(resultingHTML.length > 0)
+        if(this.newProducts.length > 0)
         {
-          document.getElementById('productsListID').innerHTML = resultingHTML + 
-            `
-            </ion-label></div><!--bindings={
-              "ng-reflect-ng-if": "false"
-            }--></div><div class="button-effect"></div>
-            `;
+          this.products = this.newProducts; 
+          this.newProducts = [];
         }
         else
-        {
-          this.newProducts = originalProducts;
-          document.getElementById('productsListID').innerHTML = this.originalHTML;
-        }
+          this.products = this.originalProducts;
       }
     });
     alert.present();
   }
 
+  sortProducts(array, sort)
+  {
+    const sortType = this.sortValue;
+    function compareToAscend(a,b) {
+      if (a[sortType] < b[sortType])
+        return -1;
+      if (a[sortType] > b[sortType])
+        return 1;
+      return 0;
+    }
+
+    function compareToDescend(a,b) {
+      if (a[sortType] > b[sortType])
+        return -1;
+      if (a[sortType] < b[sortType])
+        return 1;
+      return 0;
+    }
+
+    if(sort == 'Ascending')
+    {
+      array.sort(compareToAscend);
+    }
+
+    else if(sort == 'Descending')
+    {
+      array.sort(compareToDescend);
+    }
+  }
+
+  //arrow up/down ascending/descending
   visible = false;
+  ASCDESC = 'Ascending'
   toggle() {
     this.visible = !this.visible;
 
-    //From up to down
     if(document.getElementById('arrowUp') != null)
     {
-      console.log(document.getElementById('arrowDown'));
+      this.ASCDESC = 'Descending'
+      this.sortProducts(this.products, this.ASCDESC); 
     }
     
     if(document.getElementById('arrowDown') != null)
     {
-      console.log(document.getElementById('arrowUp'));
+      this.ASCDESC = 'Ascending'
+      this.sortProducts(this.products, this.ASCDESC); 
     }
   }
 
+  //Sort by price/stock/name
   showCheckboxSort() {
     let alert = this.alertCtrl.create();
     alert.setTitle('Sort Products By');
 
-    if(this.sortValue == 'Price')
+    if(this.sortType == 'Price')
     {
       alert.addInput({
         id: 'PRICE',
         type: 'radio',
         label: 'Price',
-        value: 'Price', 
+        value: 'PVP1', 
         checked: true
       });
     }
@@ -227,18 +226,18 @@ export class CataloguePage {
         id: 'PRICE',
         type: 'radio',
         label: 'Price',
-        value: 'Price', 
+        value: 'PVP1', 
         checked: false
       });
     }
 
-    if(this.sortValue == 'Stock')
+    if(this.sortType == 'Stock')
     {
       alert.addInput({
         id: 'STOCK',
         type: 'radio',
         label: 'Stock',
-        value: 'Stock',
+        value: 'StkActual',
         checked: true
       });
     }
@@ -248,18 +247,18 @@ export class CataloguePage {
         id: 'STOCK',
         type: 'radio',
         label: 'Stock',
-        value: 'Stock',
+        value: 'StkActual',
         checked: false
       });
     }
 
-    if(this.sortValue == 'Name')
+    if(this.sortType == 'Name')
     {
       alert.addInput({
         id: 'NAME',
         type: 'radio',
         label: 'Name',
-        value: 'Name',
+        value: 'Descricao',
         checked: true
       });
     }
@@ -269,7 +268,7 @@ export class CataloguePage {
         id: 'NAME',
         type: 'radio',
         label: 'Name',
-        value: 'Name',
+        value: 'Descricao',
         checked: false
       });
     }
@@ -279,43 +278,16 @@ export class CataloguePage {
       text: 'Okay',
       handler: data => {
         this.sortValue = data;
+        if(this.sortValue == 'PVP1')
+          this.sortType = 'Price';
+        else if(this.sortValue == 'StkActual')
+          this.sortType = 'Stock';
+        else
+          this.sortType = 'Name';
+        this.sortProducts(this.products, this.ASCDESC);
       }
     });
     alert.present();
   }
   
-}
-
-export class MyPage {
-
-  constructor(public alertCtrl: AlertController) { }
-
- /* showCheckbox() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('Which planets have you visited?');
-
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Alderaan',
-      value: 'value1',
-      checked: true
-    });
-
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Bespin',
-      value: 'value2'
-    });
-
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'Okay',
-      handler: data => {
-        console.log('Checkbox data:', data);
-        this.testCheckboxOpen = false;
-        this.testCheckboxResult = data;
-      }
-    });
-    alert.present();
-  }*/
 }
