d9d2f96c48d55ab030a01865d92ab3369af45677
up201505472 <up201505472@fe.up.pt>
2018-12-14 03:39:11 +0000

    More catalogue work

--- SalesAndGo/src/pages/catalogue/catalogue.ts
+++ SalesAndGo/src/pages/catalogue/catalogue.ts
@@ -27,9 +27,11 @@ export class CataloguePage {
   items: string[];
   sortValue: string = 'Price';
   products: string[];
+  originalHTML: string = '';
+  families: string[];
+  familiesArray: object[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
-    
     const xhttp = new XMLHttpRequest();
     xhttp.open("POST", 'http://94.60.211.16:2018/WebApi/token', true);
     var params = 'username=FEUP&password=qualquer1&company=BELAFLOR&instance=DEFAULT&grant_type=password&line=professional';
@@ -50,11 +52,13 @@ export class CataloguePage {
                        AM.PVP1, VAM.StkActual, AM.Moeda 
                        from Artigo A INNER JOIN ArtigoMoeda AM 
                        ON A.Artigo = AM.Artigo join V_INV_ArtigoArmazem VAM 
-                       on A.artigo = VAM.artigo`;
+                       on A.artigo = VAM.artigo
+                       ORDER BY AM.PVP1`;
         Http.send(JSON.stringify(query))
         Http.onreadystatechange=function(){
           if(this.readyState==4 && this.status==200){
             object.products = JSON.parse(Http.responseText).DataSet.Table;
+            object.uniq_fast(object.products);
           }
         }
       }
@@ -95,25 +99,38 @@ export class CataloguePage {
     console.log('ionViewDidLoad CataloguePage');
   }
 
+  // Returns the distinct familia element in an array
+  uniq_fast(a) {
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+          var item = a[i]['familia'];
+          if(seen[item] !== 1) {
+                seen[item] = 1;
+                out[j++] = item;
+          }
+    }
+    this.families = out;
+  }
+  
+  newProducts = [];
   showCheckbox() {
     let alert = this.alertCtrl.create();
     alert.setTitle('Select products to see');
-    let familiesArray = [];
     
     if(typeof this.products !== 'undefined')
     {
-      for(let i = 0; i < this.products.length; i++)
+      let familias = this.families;
+      for(let i = 0; i < familias.length; i++)
       {
-          let productFamily = this.products[i]['familia'];
-          if(familiesArray.indexOf(productFamily) == -1)
-          {
-            familiesArray.push(productFamily);
-            alert.addInput({
-              type: 'checkbox',
-              label: productFamily,
-              value: productFamily,
-            });
-          }
+        let familia = familias[i];
+        alert.addInput({
+          type: 'checkbox',
+          label: familia,
+          value: familia,
+        });
       }
     }
 
@@ -121,60 +138,141 @@ export class CataloguePage {
     alert.addButton({
       text: 'Okay',
       handler: data => {
+        if(this.originalHTML.length == 0)
+          this.originalHTML = document.getElementById('productsListID').innerHTML;
+        
         let originalProducts = this.products;
+        let resultingHTML = ""
         for(let i = 0; i < data.length; i++)
         {
           for(let j = 0; j < this.products.length; j++)
           {
-            if(this.products[j]['familia'] == data[i])
+            let familia = originalProducts[j]
+            if(familia['familia'] == data[i])
             {
-              console.log(document.getElementById('productsListID').innerHTML);
-             /* document.getElementById('productsListID').innerHTML = 
+              this.newProducts.push(familia);
+              resultingHTML += 
                 `
-                <ion-item>
-                  <img src = "http://94.60.211.16:8080/images/` + this.products[j]['Observacoes'] +`" style="width:40%; height:40%; display:block; margin-left: auto; margin-right:auto;">
-                  <h1 style="text-align:center; margin:8px;">` + this.products[j]['Descricao'] + `</h1>
+                <div class = "item-inner">
+                <div class = "input-wrapper">
+                <!--bindings={
+                  "ng-reflect-ng-for-of": "[object Object],[object Object"
+                }--><ion-item class="item item-block item-md"><div class="item-inner"><div class="input-wrapper"><!--bindings={
+                  "ng-reflect-ng-if": "true"
+                }--><ion-label class="label label-md">
+                  <img src = "http://94.60.211.16:8080/images/` + familia['Observacoes'] +`" style="width:40%; height:40%; display:block; margin-left: auto; margin-right:auto;">
+                  <h1 style="text-align:center; margin:8px;">` + familia['Descricao'] + `</h1>
                   <button style="float:left; width:50%; height:35px;" ion-button color="light">
-                    <h2 style="margin:auto;"><b>` + this.products[j]['PVP1'] + this.products[j]['Moeda'] +`&emsp;<ion-icon name="cart"></ion-icon></b></h2>
+                    <h2 style="margin:auto;"><b>` + familia['PVP1'] + familia['Moeda'] +`&emsp;<ion-icon name="cart" role="img" class="icon icon-md ion-md-cart" aria-label="cart" ng-reflect-name="cart"></b></h2>
                   </button>
-                  <h2 style = "float:right;margin:12px;">Stock:` + this.products[j]['StkActual'] + `</h2>
-                </ion-item>
-                `*/
+                  <h2 style = "float:right;margin:12px;">Stock:` + familia['StkActual'] + `</h2>
+                  </div>
+                  </div>
+                `
             }
           }
+        }
+        if(resultingHTML.length > 0)
+        {
+          document.getElementById('productsListID').innerHTML = resultingHTML + 
+            `
+            </ion-label></div><!--bindings={
+              "ng-reflect-ng-if": "false"
+            }--></div><div class="button-effect"></div>
+            `;
+        }
+        else
+        {
+          this.newProducts = originalProducts;
+          document.getElementById('productsListID').innerHTML = this.originalHTML;
         }
       }
     });
     alert.present();
   }
 
+  visible = false;
+  toggle() {
+    this.visible = !this.visible;
+
+    //From up to down
+    if(document.getElementById('arrowUp') != null)
+    {
+      console.log(document.getElementById('arrowDown'));
+    }
+    
+    if(document.getElementById('arrowDown') != null)
+    {
+      console.log(document.getElementById('arrowUp'));
+    }
+  }
+
   showCheckboxSort() {
     let alert = this.alertCtrl.create();
     alert.setTitle('Sort Products By');
 
-    alert.addInput({
-      id: 'PRICE',
-      type: 'radio',
-      label: 'Price',
-      value: 'Price', 
-      checked: true
-    });
+    if(this.sortValue == 'Price')
+    {
+      alert.addInput({
+        id: 'PRICE',
+        type: 'radio',
+        label: 'Price',
+        value: 'Price', 
+        checked: true
+      });
+    }
+    else
+    {
+      alert.addInput({
+        id: 'PRICE',
+        type: 'radio',
+        label: 'Price',
+        value: 'Price', 
+        checked: false
+      });
+    }
 
-    alert.addInput({
-      id: 'STOCK',
-      type: 'radio',
-      label: 'Stock',
-      value: 'Stock',
-      checked: false
-    });
+    if(this.sortValue == 'Stock')
+    {
+      alert.addInput({
+        id: 'STOCK',
+        type: 'radio',
+        label: 'Stock',
+        value: 'Stock',
+        checked: true
+      });
+    }
+    else
+    {
+      alert.addInput({
+        id: 'STOCK',
+        type: 'radio',
+        label: 'Stock',
+        value: 'Stock',
+        checked: false
+      });
+    }
 
-    alert.addInput({
-      id: 'NAME',
-      type: 'radio',
-      label: 'Name',
-      value: 'Name',
-      checked: false
-    });
+    if(this.sortValue == 'Name')
+    {
+      alert.addInput({
+        id: 'NAME',
+        type: 'radio',
+        label: 'Name',
+        value: 'Name',
+        checked: true
+      });
+    }
+    else
+    {
+      alert.addInput({
+        id: 'NAME',
+        type: 'radio',
+        label: 'Name',
+        value: 'Name',
+        checked: false
+      });
+    }
 
     alert.addButton('Cancel');
     alert.addButton({
