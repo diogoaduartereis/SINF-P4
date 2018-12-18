bf9a97f9d5393b8c240c1821d748acb0e3fe2677
Maria de Abreu <mariadsda@gmail.com>
2018-12-18 01:46:43 +0000

    checkout 90% done, so falta os documentos porque mudaram

--- SalesAndGo/src/pages/checkout/checkout.ts
+++ SalesAndGo/src/pages/checkout/checkout.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { PrimaveraProvider } from '../../providers/primavera/primavera';
+import { CataloguePage} from '../catalogue/catalogue';
 
 /**
  * Generated class for the ClientPage page.
@@ -15,80 +16,88 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'page-checkout',
   templateUrl: 'checkout.html',
+  providers: [PrimaveraProvider],
 })
 export class CheckoutPage {
-  clients:any=[];
+  clients: any = [];
+  selectedClient : any = {Fac_Mor:'', Moeda:''};
+  isEnabled: boolean = false;
   token: string;
-  //products:any=[{Descricao:"Marcador"},{Descricao:"Pencil"}];
-  products:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public http: HttpClient) {
+  moeda: string;
+  products: string[] = [];
+  total: any = 0;
+  primaveraAccess: any;
+  access_token:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+    public http: HttpClient, public primavera: PrimaveraProvider) {
     this.products = navParams.get('products');
-    console.log(this.products);
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", 'http://94.60.211.16:2018/WebApi/token', true);
-    var params = 'username=FEUP&password=qualquer1&company=BELAFLOR&instance=DEFAULT&grant_type=password&line=professional';
-    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    xhttp.send(params);
-    var classthis = this;
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        classthis.token = JSON.parse(xhttp.responseText).access_token;
-        console.log(classthis.token);
-
-        const options = {
-          headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + classthis.token,
-          }),
-        };
-        // get clients
-        let query = `"SELECT V.Vendedor, V.Nome, C.Cliente FROM Vendedores V INNER JOIN Clientes C ON C.Vendedor = V.Vendedor WHERE C.Vendedor = '1' ORDER BY V.Vendedor"`;
-        classthis.http.post("http://94.60.211.16:2018/WebApi/Administrador/Consulta", query, options)
-          .subscribe((response : any) => {
-            classthis.clients = response.DataSet.Table;
-            console.log(classthis.clients);
-          });
-       /* // get products
-        classthis.http.get("http://94.60.211.16:2018/WebApi/Base/Artigos/LstArtigos", options)
-          .subscribe((response : any) => { 
-            classthis.products = response.DataSet.Table;
-            console.log(classthis.products);
-          });*/
+    if(this.products)
+      for (var i=0; i<this.products.length; i++)
+      {
+        this.moeda=this.products[i]['Moeda'];
+        this.total+=this.products[i]['PVP1']*this.products[i]['quantidade'];
       }
+    this.primaveraAccess=primavera;
+    this.access_token = primavera.genAccessToken();
+
+    // get clients  EDITAR com ID DO VENDEDOR Q ESTIVER LOGGED IN
+    let query = `SELECT V.Vendedor, C.Nome, C.Cliente, C.Fac_Mor, C.Moeda FROM Vendedores V INNER JOIN Clientes C ON C.Vendedor = V.Vendedor WHERE C.Vendedor = 'SOF' ORDER BY V.Vendedor`;
+    let response = primavera.postRequest(this.access_token, '/Administrador/Consulta', 200, query);
+    if (typeof response != 'undefined') {
+      this.clients = response;
+    }
+    console.log(this.clients);
+  }
+  
+  decrementProduct(product){
+    let index = this.products.indexOf(product);
+    if(index!=-1){
+      this.products[index]['quantidade']--;
+      this.total=this.total-this.products[index]['PVP1'];
+      this.products[index]['StkActual']++;
+      if(this.products[index]['quantidade']===0){
+        this.products.splice(index,1)
+        if(this.products.length===0)
+          this.navCtrl.push(CataloguePage,{});
+      }
+      else  this.products[index]['isPlusEnabled']=true;
+       //updateTotalPrice
     }
   }
 
-  insertDocumento(event: any, docType) {
-    if (!this.token)
-      return;
-    let document, linha1, linha2;
+  incrementProduct(product){
+    product['quantidade']++;
+    this.total=this.total+product['PVP1'];
+    product['StkActual']--;
+    if(product['StkActual']===0)
+      product['isPlusEnabled']=false;
+    //updateTotalPrice
+  }
+
+  onSelectClient(){
+    this.isEnabled=true;
+  }
+
+  insertDocumento(products, docType) {
+    let document;
     let linhas = [];
-    linha1 = {
-      Artigo: "A0001",
-      Quantidade: 50
+
+    for (var i=0; i < products.length; i++)
+    {
+      let linha = { Artigo: products[i].Artigo, Quantidade: products[i].quantidade };
+      linhas.push(linha);
     }
-    linha2 = {
-      Artigo: "A0002",
-      Quantidade: 50
-    }
-    linhas.push(linha1);
-    linhas.push(linha2);
     document = {
       Linhas: linhas,
       Tipodoc: docType,
       Serie: "A",
-      Entidade: "C0003",
+      Entidade: this.selectedClient,
       TipoEntidade: "C"
     }
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.token,
-      })
-    };
-    this.http.post("http://94.60.211.16:2018/WebApi/Vendas/Docs/CreateDocument/", document, httpOptions)
-      .subscribe(response => console.log(response));
+    let response = this.primaveraAccess.postRequest(this.access_token, '/Vendas/Docs/CreateDocument/', 203, document);
+    if (typeof response != 'undefined') {
+      console.log(response)
+      //  this.navCtrl.push(CataloguePage,{});
+    }
   }
 }
